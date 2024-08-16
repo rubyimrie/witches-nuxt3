bf1cc1bbf74944ff40f145646a8e5e8e3ab68fb4
Ruby Imrie <s2056921@ed.ac.uk>
2024-08-16 10:11:08 +0100

    create boiler plate, migrate head, runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  axios: {
+    // extra config e.g
+    baseURL: process.env.NODE_ENV === 'development' ? "http://localhost:8181" : 'https://witches.is.ed.ac.uk'
+  },
+  app:{
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  }
+},
 })
