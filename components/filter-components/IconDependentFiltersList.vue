483b0b563d5ab47ad6fd857b0e5cdd6088901da4
Ruby Imrie <s2056921@ed.ac.uk>
2024-09-02 14:30:48 +0100

    custom css for checkbox

--- components/filter-components/IconDependentFiltersList.vue
+++ components/filter-components/IconDependentFiltersList.vue
@@ -1,60 +1,55 @@
 <template>
   <div class="w-full">
-    <div v-if="property === currentProperty" 
-        class="w-full flex flex-wrap mt-2">
+    <div v-if="property === currentProperty" class="w-full flex flex-wrap mt-2">
       <div v-for="(filterItem, filterType) in sortedFiltersList"
-          class="flex flex-col items-center mx-3 mb-2" 
-          style="width: 50px">
+          class="flex flex-col items-center mx-3 mb-2" style="width: 50px">
 
         <div class="flex justify-center items-center">
-          <input :checked="filterItem.active"
-                @change="filterEmit(filterType)" 
-                type="checkbox" />
+          <input
+            @change="filterEmit(filterType)"
+            :checked="filterItem.active"
+            type="checkbox"
+            class="w-4 h-4 appearance-none bg-gray-100 border border-gray-300 rounded-sm checked:bg-blue-600 checked:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-0 dark:bg-gray-700 dark:border-gray-600
+                  checked:before:content-['✓'] checked:before:text-white checked:before:block checked:before:text-center checked:before:text-xs checked:before:font-bold checked:before:leading-none"
+          />
           <img class="witch-icon mb-1 ml-0.5" :src="filterItem.iconUrl" />
         </div>
         <p class="text-xs text-center"> {{ filterItem.label }} </p>
-
       </div>
     </div>
 
-    <!-- Else, show list without icons but with button to switch
-        to current. -->
+    <!-- Else, show list without icons but with button to switch to current. -->
     <div v-else class="flex flex-col items-start w-full mt-2 mb-2">
       <div class="w-full flex flex-wrap px-2">
         <div v-for="(filterItem, filterType) in sortedFiltersList">
           <div class="flex mb-3 mx-2 w-full items-center">
-
-            <input :checked="filterItem.active"
-                   @change="filterEmit(filterType)" 
-                   type="checkbox" />
-            <p class="text-xs text-center ml-1">
-              {{ filterItem.label }}
-            </p>
-
+            <input
+            @change="filterEmit(filterType)"
+            :checked="filterItem.active"
+            type="checkbox"
+            class="w-4 h-4 appearance-none bg-gray-100 border border-gray-300 rounded-sm checked:bg-blue-600 checked:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-0 dark:bg-gray-700 dark:border-gray-600
+                  checked:before:content-['✓'] checked:before:text-white checked:before:block checked:before:text-center checked:before:text-xs checked:before:font-bold checked:before:leading-none"
+          />
+          <p class="text-xs text-center ml-1"> {{ filterItem.label }} </p>
           </div>
+          
         </div>
       </div>
       <!-- Change icons button. -->
       <div class="flex flex-col w-4/5 ml-1 items-end mt-1">
-        <div class="self-end flex justify-start items-center
-                    mb-1 mr-1">
-          <p class="text-xs">
-            Show {{ propertyLabel }} Icons
-          </p>
-          <label class="container flex items-center
-                        justify-center ml-1">
-            <input type="radio" :checked="false" 
-                   name="radio" @change="emitSetPropertyToCurrent(property)">
+        <div class="self-end flex justify-start items-center mb-1 mr-1">
+          <p class="text-xs">Show {{ propertyLabel }} Icons</p>
+          <label class="container flex items-center justify-center ml-1">
+            <input type="radio" :checked="false" name="radio" @change="emitSetPropertyToCurrent(property)" />
             <span class="checkmark"></span>
           </label>
         </div>
-        <div class="w-full bg-slate-600" 
-            style="height:1.2px;">
-        </div>
+        <div class="w-full bg-slate-600" style="height:1.2px;"></div>
       </div>
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
--- components/filter-components/NormalFiltersList.vue
+++ components/filter-components/NormalFiltersList.vue
@@ -3,10 +3,12 @@
     <div v-for="(filterItem, filterType) in sortedFiltersList" :key="filterType">
       <div class="flex mb-3 mx-2 w-full items-center">
         <input
-          :checked="filterItem.active"
-          @change="filterEmit(filterType)"
-          type="checkbox"
-        />
+                  @change="filterEmit(filterType)"
+                  :checked="filterItem.active"
+                  type="checkbox"
+                  class="w-4 h-4 appearance-none bg-gray-100 border border-gray-300 rounded-sm checked:bg-blue-600 checked:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-0 dark:bg-gray-700 dark:border-gray-600
+                        checked:before:content-['✓'] checked:before:text-white checked:before:block checked:before:text-center checked:before:text-xs checked:before:font-bold checked:before:leading-none"
+                  />
         <p class="text-xs text-center ml-1">
           {{ filterItem.label }}
         </p>
